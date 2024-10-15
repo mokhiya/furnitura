2d9655e9377f8528e203a3d66a8b17c7a8f6648d
mokhiya <mohiyaxonuzokova@gmail.com>
2024-10-16 04:08:24 +0500

    next commit

--- pages/views.py
+++ pages/views.py
@@ -3,6 +3,42 @@ from django.views.generic import TemplateView
 
 
 class ContentView(TemplateView):
-    template_name = 'layouts/base.html'
+    template_name = 'contact.html'
 
-# Create your views here.
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+
+# blogs
+class BlogListView(TemplateView):
+    template_name = 'blog-list.html'
+
+
+class BlogDetailView(TemplateView):
+    template_name = 'blog-detail.html'
+
+
+# pages
+class ProductListView(TemplateView):
+    template_name = 'product-list.html'
+
+
+class ProductDetailView(TemplateView):
+    template_name = 'product-detail.html'
+
+
+class AboutUsView(TemplateView):
+    template_name = 'about-us.html'
+
+
+class CartView(TemplateView):
+    template_name = 'cart.html'
+
+
+class WishlistView(TemplateView):
+    template_name = 'user-wishlist.html'
+
+
+class CheckoutView(TemplateView):
+    template_name = 'checkout.html'
