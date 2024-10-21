194ef6bdbd7336b97349007880bb364e241cd749
mokhiya <mohiyaxonuzokova@gmail.com>
2024-10-21 09:19:04 +0500

    add code for product list html

--- products/views.py
+++ products/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 
-from products.models import ProductModel
+from products.models import ProductModel, CategoryModel, TagModel, BrandModel, ColorModel
 
 
 class ProductListView(ListView):
-    template_name = 'products/product-list.html'
     model = ProductModel
+    template_name = 'products/product-list.html'
     context_object_name = 'products'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = CategoryModel.objects.all()
+        context['tags'] = TagModel.objects.all()
+        context['colors'] = ColorModel.objects.all()
+        context['brands'] = BrandModel.objects.all()
+        return context
 
-class ProductDetailView(TemplateView):
+
+class ProductDetailView(DetailView):
+    model = ProductModel
     template_name = 'products/product-detail.html'
+    context_object_name = 'products'
